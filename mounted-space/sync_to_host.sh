7c8b467a8ab96b936fe594aa7232400c91b6604a
Unknown <arvy@rvbd.net>
2019-08-12 23:17:37 +0700

    Allow specific app sync via bash positional parameter

--- mounted-space/sync_to_host.sh
+++ mounted-space/sync_to_host.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-rsync -ru --inplace --exclude='.git/' --exclude='bench.egg-info/' --exclude='.idea/' --exclude='pids/' --progress /home/vagrant/app/ /mounted-space/app/
+SECONDS=0 ;
 
+if [ "$1" != "" ]; then
+    echo "Sync specific frappe app only"
+    rsync -ru --exclude='.git/' --exclude='bench.egg-info/' --exclude='.idea/' --exclude='pids/' --inplace --progress /home/vagrant/app/frappe-bench/apps/"$1"/ /mounted-space/app/frappe-bench/apps/"$1"/ 
+else
+    echo "Sync all"
+    rsync -ru --exclude='.git/' --exclude='bench.egg-info/' --exclude='.idea/' --exclude='pids/' --inplace --progress  /home/vagrant/app/ /mounted-space/app/
+fi
+
+echo "Elapsed time:" $SECONDS "seconds"
--- mounted-space/sync_to_vm.sh
+++ mounted-space/sync_to_vm.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-rsync -ru --exclude='.git/' --exclude='bench.egg-info/' --exclude='.idea/' --exclude='pids/' --inplace --progress /mounted-space/app/ /home/vagrant/app/ 
+SECONDS=0 ;
+
+if [ "$1" != "" ]; then
+    echo "Sync specific frappe app only"
+    rsync -ru --exclude='.git/' --exclude='bench.egg-info/' --exclude='.idea/' --exclude='pids/' --inplace --progress /mounted-space/app/frappe-bench/apps/"$1"/ /home/vagrant/app/frappe-bench/apps/"$1"/
+else
+    echo "Sync all"
+    rsync -ru --exclude='.git/' --exclude='bench.egg-info/' --exclude='.idea/' --exclude='pids/' --inplace --progress /mounted-space/app/ /home/vagrant/app/ 
+fi
 
 cd /home/vagrant/app/frappe-bench/ && bench migrate 
 
 cd /mounted-space/
+
+echo "Elapsed time:" $SECONDS "seconds"
